858868912751f8efcf7707606448bbadd776f9ed
liujinxin <xinxin_949421@yahoo.com>
2021-03-10 10:35:30 +0800

    Signed-off-by: liujinxin <xinxin_949421@yahoo.com>

--- base/demo03.py
+++ base/demo03.py
@@ -7,7 +7,7 @@ import sys
 
 workDay = 0
 restDay = 0
-month = 1
+month = 4
 weeks = calendar.monthcalendar(2021, month)
 
 jobDays = [[] for i in range(6)]
@@ -15,9 +15,10 @@ jobDaysIndex = 0
 
 print('# ' + str(month) + '月日报')
 print()
-
-print('星期一 | 星期二 | 星期三 | 星期四 | 星期五 | <font color=red>星期六</font> | <font color=red>星期日</font>')
-print('---|---|---|---|---|---|---')
+print('## 一、日历')
+print()
+print('| 星期一 | 星期二 | 星期三 | 星期四 | 星期五 | <font color=red>星期六</font> | <font color=red>星期日</font> | 项目统计 |')
+print('|---|---|---|---|---|---|---|---|')
 
 for week in weeks:
     for i in range(len(week)):
@@ -35,45 +36,30 @@ for week in weeks:
             jobDays[jobDaysIndex].append(day)
         #print()
         sys.stdout.write(week[i])
+    sys.stdout.write(' |')
     print()
 #print(weeks)
 
 print()
-
-print('日历统计（天）：')
+print('## 二、月度统计')
 print()
-print('<font color=SeaGreen>工作</font> | <font color=Red>休息</font> | <font color=#CC33CC>加班</font> | <font color=DeepSkyBlue>出差</font> | <font color=IndianRed>年假</font> | <font color=#DF0101>倒休</font>')
-print('-------|--------|--------|------|------|------')
-print('<font color=SeaGreen>'+str(workDay)+'</font> | <font color=Red>'+str(restDay)+'</font> | <font color=#CC33CC>0</font> | <font color=DeepSkyBlue>0</font> | <font color=IndianRed>0</font> | <font color=#DF0101>0</font>')
-
-print()
-print('工时统计：')
-print()
-print('items | 实际 | 法定要求')
-print('-------|--------|------')
-print('出勤天数 | 0 | '+str(workDay))
-print('平均工时 | 0 | 8')
-print('总工时 | 0 | '+str(workDay*8))
-print('使用倒休小时 | 0 | -')
-print('获得倒休小时 | 0 | -')
+print('| 工时统计 <br/> 实际 / 法定 | 类别统计（天） | 项目统计（天）|')
+print('|---|---|---|')
+print('| 出勤天数 <br/> 0 / '+str(workDay)+' | <font color=SeaGreen>工作：0</font>     | |')
+print('| 平均工时 <br/> 0 / 8  | <font color=Red>休息：0</font>          | |')
+print('| 总工时 <br/> 0 / '+str(workDay*8)+'  | <font color=#CC33CC>加班：0</font>      | |')
+print('|                       | <font color=DeepSkyBlue>出差：0</font>  | |')
+print('| 使用年假 <br/> 0 小时 | <font color=IndianRed>年假：0</font>    | |')
+print('| 使用倒休 <br/> 0 小时 | <font color=#DF0101>倒休：3</font>      | |')
+print('| 获得倒休 <br/> 0 小时 |                                         | |')
 
 print()
-print('工作日汇总统计（天）：')
-print()
-print('星期\\项目 | XXX | 汇总')
-print('----------|-----|------')
-for jobDay in jobDays:
-    if len(jobDay) > 0:
-        print(str(jobDay) + ' | 0.0 | 0.0')
-print('汇总    | 0.0 | 0.0')
-
-print()
-print('详情：')
+print('## 三、详情')
 
 for jobDay in jobDays:
     if len(jobDay) > 0:
         print()
-        print('## ' + str(jobDay))
+        print('### ' + str(jobDay))
         print()
         for day in jobDay:
             print('- ' + str(day) + "：XXX")
